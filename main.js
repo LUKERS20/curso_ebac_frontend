404f2e294153d16afd648344d61f8875ed1538b0
Lucas Silva <lucas1w34@gmail.com>
2023-02-24 10:49:46 -0300

    atividade

---
+++ main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form-validar');
+const valorA = document.getElementById('campo-A');
+const valorB = document.getElementById('campo-B');
+let formEvalido = false;
+
+function validanumero(valorA, valorB) {
+    return valorB > valorA
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const mensagemSucesso = `Formulário validado com sucesso! <b>Valor de A: ${valorA.value}</b> é menor que o <b>Valor de B: ${valorB.value}</b>`;
+    const mensagemErro = `Formulário negado! <b>Valor de A: ${valorA.value}</b> é maior que o <b>Valor de B: ${valorB.value}</b>`;
+
+
+    formEvalido = validanumero(valorA.value, valorB.value)
+    if (formEvalido) {
+        const containerMensagemSucesso =  document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+        document.querySelector('.error-message').style.display = 'none';
+        valorA.style.border = 'none';
+        valorB.style.border = 'none';
+
+        valorA.value = '';
+        valorB.value = '';
+    } else {
+        const containerMensagemErro =  document.querySelector('.error-message');
+        containerMensagemErro.innerHTML = mensagemErro;
+        valorA.style.border = '2px solid red';
+        valorB.style.border = '2px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+        document.querySelector('.success-message').style.display = 'none'
+        
+
+        valorA.value = '';
+        valorB.value = '';
+    }
+})
+
